7a3a048b7cf9ebfcd850acd501e4715f0fc25909
Sandeep Chaurasiya <chaurasiyasandeep9140@gmail.com>
2026-09-22 08:15:33 +0530

    Add files via upload

---
+++ My-project3/src/main/java/com/sandeep/MyProject3Application.java
@@ -0,0 +1,13 @@
+package com.sandeep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyProject3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyProject3Application.class, args);
+	}
+
+}
